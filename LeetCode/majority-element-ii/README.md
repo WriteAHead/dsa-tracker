b5a2917f9ba15a51bf1e76942c4db6b4fe100ff9
Subham <211491751+WriteAHead@users.noreply.github.com>
2026-08-13 01:48:23 +0000

    auto: sync latest LeetCode & Codeforces submissions

---
+++ LeetCode/majority-element-ii/README.md
@@ -0,0 +1,5 @@
+# Majority Element II
+
+- **Problem Link:** [https://leetcode.com/problems/majority-element-ii/](https://leetcode.com/problems/majority-element-ii/)
+- **Status:** Accepted
+- **Submission ID:** 2104555139
---
+++ LeetCode/majority-element-ii/solution.py
@@ -0,0 +1,3 @@
+# Solution for LeetCode problem: Majority Element II
+# Problem Link: https://leetcode.com/problems/majority-element-ii/
+# Note: Full automated source code retrieval for LeetCode requires session authentication.
